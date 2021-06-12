fc70944606c05ed8924d3244839f68d7247ee368
VicCastro80 <victor.castro@outlook.com>
2021-06-12 16:58:22 -0500

    Create Driver.cs

---
+++ RestAPI/CodeKata_Drivers/Models/Driver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeKata_Drivers.Models
+{
+    public class Driver
+    {
+        public string driverName { get; set; }
+        public double TotalMiles { get; set; }
+        public double avgSpeed { get; set; }
+        public List<Trip> trips { get; set; }
+    }
+}
