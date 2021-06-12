d965e3041e86fc5fff955f0eda6957b70c5aa5f7
VicCastro80 <victor.castro@outlook.com>
2021-06-12 16:58:29 -0500

    Create Trip.cs

---
+++ RestAPI/CodeKata_Drivers/Models/Trip.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeKata_Drivers.Models
+{
+    public class Trip
+    {
+        public string driverName { get; set; }
+        public DateTime startTime { get; set; }
+        public DateTime endTime { get; set; }
+        public double tripTime { get; set; }
+        public double milesDriven { get; set; }
+    }
+}
