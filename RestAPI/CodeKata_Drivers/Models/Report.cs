26482d15cf9262f9b62df5d9fc0c960b79aca640
VicCastro80 <victor.castro@outlook.com>
2021-06-12 16:58:26 -0500

    Create Report.cs

---
+++ RestAPI/CodeKata_Drivers/Models/Report.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeKata_Drivers.Models
+{
+    public class Report
+    {
+        public string reportLine { get; set; }
+    }
+}
